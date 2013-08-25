fb50852f4377c5c18e39a4f4955a4d6a81846e17
Okan Tanrikulu <motanrikulu@gmail.com>
2013-08-25 10:49:43 -0700

    first commit of first_app using RubyMine and Rails 3 tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '2aaa93760a3e1de33d004c10833fef84d241253bec4d8e4a6c72c36e206feca1eae79d5b11683c093b68f2224bb65fac534904a2c4ca11c150d51d2036aded97'
